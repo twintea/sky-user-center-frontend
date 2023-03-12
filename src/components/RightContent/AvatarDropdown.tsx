3e95e1311e67ede167044c94376abe5f74e4f016
twintee <twintee@git.com>
2023-03-12 15:25:21 +0800

    update project name & fix some bugs

--- src/components/RightContent/AvatarDropdown.tsx
+++ src/components/RightContent/AvatarDropdown.tsx
@@ -1,11 +1,11 @@
-import {outLogin} from '@/services/ant-design-pro/api';
-import {LogoutOutlined, SettingOutlined, UserOutlined} from '@ant-design/icons';
-import {Avatar, Menu, Spin} from 'antd';
-import type {ItemType} from 'antd/lib/menu/hooks/useItems';
-import {stringify} from 'querystring';
-import type {MenuInfo} from 'rc-menu/lib/interface';
-import React, {useCallback} from 'react';
-import {history, useModel} from 'umi';
+import { outLogin } from '@/services/ant-design-pro/api';
+import { LogoutOutlined, SettingOutlined, UserOutlined } from '@ant-design/icons';
+import { Avatar, Menu, Spin } from 'antd';
+import type { ItemType } from 'antd/lib/menu/hooks/useItems';
+import { stringify } from 'querystring';
+import type { MenuInfo } from 'rc-menu/lib/interface';
+import React, { useCallback } from 'react';
+import { history, useModel } from 'umi';
 import HeaderDropdown from '../HeaderDropdown';
 import styles from './index.less';
 
@@ -18,27 +18,29 @@ export type GlobalHeaderRightProps = {
  */
 const loginOut = async () => {
   await outLogin();
-  const {query = {}, search, pathname} = history.location;
-  const {redirect} = query;
-  // Note: There may be security issues, please note
-  if (window.location.pathname !== '/user/login' && !redirect) {
-    history.replace({
-      pathname: '/user/login',
-      search: stringify({
-        redirect: pathname + search,
-      }),
-    });
-  }
+  // const {query = {}, search, pathname} = history.location;
+  // const {redirect} = query;
+  // // Note: There may be security issues, please note
+  // if (window.location.pathname !== '/user/login' && !redirect) {
+  //   history.replace({
+  //     pathname: '/user/login',
+  //     search: stringify({
+  //       redirect: pathname + search,
+  //     }),
+  //   });
+  // }
+  //推出登录，直接回到登录页，不保存当前url
+  history.push('/user/login');
 };
 
-const AvatarDropdown: React.FC<GlobalHeaderRightProps> = ({menu}) => {
-  const {initialState, setInitialState} = useModel('@@initialState');
+const AvatarDropdown: React.FC<GlobalHeaderRightProps> = ({ menu }) => {
+  const { initialState, setInitialState } = useModel('@@initialState');
 
   const onMenuClick = useCallback(
     (event: MenuInfo) => {
-      const {key} = event;
+      const { key } = event;
       if (key === 'logout') {
-        setInitialState((s) => ({...s, currentUser: undefined}));
+        setInitialState((s) => ({ ...s, currentUser: undefined }));
         loginOut();
         return;
       }
@@ -63,7 +65,7 @@ const AvatarDropdown: React.FC<GlobalHeaderRightProps> = ({menu}) => {
     return loading;
   }
 
-  const {currentUser} = initialState;
+  const { currentUser } = initialState;
 
   if (!currentUser || !currentUser.userName) {
     return loading;
@@ -72,36 +74,41 @@ const AvatarDropdown: React.FC<GlobalHeaderRightProps> = ({menu}) => {
   const menuItems: ItemType[] = [
     ...(menu
       ? [
-        {
-          key: 'center',
-          icon: <UserOutlined/>,
-          label: '个人中心',
-        },
-        {
-          key: 'settings',
-          icon: <SettingOutlined/>,
-          label: '个人设置',
-        },
-        {
-          type: 'divider' as const,
-        },
-      ]
+          {
+            key: 'center',
+            icon: <UserOutlined />,
+            label: '个人中心',
+          },
+          {
+            key: 'settings',
+            icon: <SettingOutlined />,
+            label: '个人设置',
+          },
+          {
+            type: 'divider' as const,
+          },
+        ]
       : []),
     {
       key: 'logout',
-      icon: <LogoutOutlined/>,
+      icon: <LogoutOutlined />,
       label: '退出登录',
     },
   ];
 
   const menuHeaderDropdown = (
-    <Menu className={styles.menu} selectedKeys={[]} onClick={onMenuClick} items={menuItems}/>
+    <Menu className={styles.menu} selectedKeys={[]} onClick={onMenuClick} items={menuItems} />
   );
 
   return (
     <HeaderDropdown overlay={menuHeaderDropdown}>
       <span className={`${styles.action} ${styles.account}`}>
-        <Avatar size="small" className={styles.avatar} src={currentUser.avatarUrl} alt="avatarUrl"/>
+        <Avatar
+          size="small"
+          className={styles.avatar}
+          src={currentUser.avatarUrl}
+          alt="avatarUrl"
+        />
         <span className={`${styles.name} anticon`}>{currentUser.userName}</span>
       </span>
     </HeaderDropdown>
--- src/plugins/gloabalRequest.ts
+++ src/plugins/gloabalRequest.ts
@@ -2,11 +2,10 @@
  * request 网络请求工具
  * 更详细的 api 文档: https://github.com/umijs/umi-request
  */
-import {extend} from 'umi-request';
-import {message} from "antd";
-import {stringify} from "querystring";
-import {history} from "umi";
-
+import { extend } from 'umi-request';
+import { message } from 'antd';
+import { stringify } from 'querystring';
+import { history } from 'umi';
 
 /**
  * 配置request请求时的默认参数
@@ -14,7 +13,7 @@ import {history} from "umi";
 const request = extend({
   credentials: 'include', // 默认请求是否带上cookie
   //根据环境不同选择不同的请求地址
-  prefix: process.env.NODE_ENV === 'production' ? 'http://skyuc.twintea.top' : undefined
+  prefix: process.env.NODE_ENV === 'production' ? 'http://skyuc-backend.twintea.top' : undefined,
   // requestType: 'form',
 });
 
@@ -35,13 +34,12 @@ request.interceptors.request.use((url, options): any => {
  * 所有响应拦截器
  */
 request.interceptors.response.use(async (response, options): Promise<any> => {
-
   const res = await response.clone().json();
   if (res.code === 0) {
     return res.data;
   }
   if (res.code === 40100) {
-    message.error("请先登录");
+    message.error('请先登录');
     history.replace({
       pathname: '/user/login',
       search: stringify({
@@ -51,7 +49,6 @@ request.interceptors.response.use(async (response, options): Promise<any> => {
   } else {
     message.error(res.description);
   }
-})
-;
+});
 
 export default request;
